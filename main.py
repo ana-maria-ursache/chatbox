545c80e3ca9134773cc93f74d23d829a15fcbed3
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-03-09 16:45:31 +0200

    feat: added sqlalchemy

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+# ----------------------------------------
 def main():
-    print("Hello from chatbox!")
+    # print("Hello from chatbox!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
