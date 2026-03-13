4ba49bdce0d5b3e21a17e5fbbf4337a3c7e8967a
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-03-13 10:33:22 +0200

    feat:add files and preview them

--- db/models.py
+++ db/models.py
@@ -1,13 +1,8 @@
-from sqlalchemy import (
-    Boolean,
-    Column,
-    DateTime,
-    Integer,
-    String,
-    func,
-)
+from sqlalchemy import Column, DateTime, Integer, String, func, ForeignKey
+from sqlalchemy.orm import relationship
 
 from db.database import Base
+
 
 class UserRecord(Base):
     __tablename__ = "users"
@@ -15,7 +10,21 @@ class UserRecord(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     name = Column(String, nullable=False)
-    avatar_url = Column(String, nullable=False)
-    password_hash = Column(String, nullable=False) 
+    avatar_url = Column(String, nullable=True)
+    password_hash = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    
+
+
+class FileRecord(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    nume = Column(String, nullable=False)
+    generated_name = Column(String, unique=True, nullable=False)
+    content_type = Column(String, nullable=True) 
+    path = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # for linking the object, not only the id + file.owner.name/email
+    owner = relationship("UserRecord")
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import uvicorn
-from db.models import UserRecord
 from db.database import Base, engine
-from sqlalchemy.orm import Session
-from api.routes import core_router, auth_router
+from api.routes import core_router, auth_router, files_router
 from api.lifespan import lifespan
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(lifespan = lifespan)
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(core_router)
 app.include_router(auth_router)
+app.include_router(files_router)
+
 
 # ----------------------------------------
 def main():
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
 
 if __name__ == "__main__":
     main()
